14d85c132ad123288c12b35b6273e0f9d66e37ac
lav1510 <maria..bugarin@student.upt.ro>
2023-03-16 16:00:33 +0200

    replaced front porch and back porch with margins

--- VGA_Control/Parameters.v
+++ VGA_Control/Parameters.v
@@ -1,33 +1,37 @@
 	//Parameters are taken from https://web.mit.edu/6.111/www/s2004/NEWKIT/vga.shtml, for the 60Hz
 	
-	//Parameters for front porch
-	parameter [PORCH_WIDTH-1:0] H_Front_Porch_RD      =  8'b00010000;   //16
-	parameter [PORCH_WIDTH-1:0] V_Front_Porch_RD      =  8'b00001011;   //11
-    parameter [PORCH_WIDTH-1:0] H_Front_Porch_R8x6    =  8'b00101000;   //40
-	parameter [PORCH_WIDTH-1:0] V_Front_Porch_R8x6    =  8'b00000001;   //1
-    parameter [PORCH_WIDTH-1:0] H_Front_Porch_R10x7   =  8'b00011000;   //24
-    parameter [PORCH_WIDTH-1:0] V_Front_Porch_R10x7   =  8'b00000011;   //3
+	//Parameters for left margin, where left margin means front porch + sync pulse
+	parameter [VL_MARGIN_WIDTH-1:0] V_Left_Margin_RD      =  4'b1101;   	//11+  2= 13
+	parameter [VL_MARGIN_WIDTH-1:0] V_Left_Margin_R8x6    =  4'b0101;   	// 1+  4=  5
+    parameter [VL_MARGIN_WIDTH-1:0] V_Left_Margin_R10x7   =  4'b1001;   	// 3+  6=  9
 	
-	//Parameters for back porch
-	parameter [PORCH_WIDTH-1:0] H_Back_Porch_RD     =  8'b00110000;   //48
-	parameter [PORCH_WIDTH-1:0] V_Back_Porch_RD     =  8'b00011111;   //31
-    parameter [PORCH_WIDTH-1:0] H_Back_Porch_R8x6   =  8'b01011000;   //88
-	parameter [PORCH_WIDTH-1:0] V_Back_Porch_R8x6   =  8'b00010111;	  //23
-    parameter [PORCH_WIDTH-1:0] H_Back_Porch_R10x7  =  8'b10100000;   //160
-    parameter [PORCH_WIDTH-1:0] V_Back_Porch_R10x7  =  8'b00011101;   //29
+	parameter [HL_MARGIN_WIDTH-1:0] H_Left_Margin_RD      =  8'b01110000;   //16+ 96=112
+    parameter [HL_MARGIN_WIDTH-1:0] H_Left_Margin_R8x6    =  8'b10101000;   //40+128=168
+    parameter [HL_MARGIN_WIDTH-1:0] H_Left_Margin_R10x7   =  8'b10100000;   //24+136=160
+
+	
+	//Parameters for right margin, where right margin means maximum counters - back porch (or sync pulse + front porch + acive) 
+	parameter [VR_MARGIN_WIDTH-1:0] V_Right_Margin_RD     =  10'b0111101101;   // 524- 31=755
+	parameter [VR_MARGIN_WIDTH-1:0] V_Right_Margin_R8x6   =  10'b1001011101;	// 628- 23=605
+    parameter [VR_MARGIN_WIDTH-1:0] V_Right_Margin_R10x7  =  10'b1100001001;   // 806- 29=777
+
+	parameter [HR_MARGIN_WIDTH-1:0] H_Right_Margin_RD     =  11'b01011110000;   // 800- 48=752
+    parameter [HR_MARGIN_WIDTH-1:0] H_Right_Margin_R8x6   =  11'b01111001000;   //1056- 88=968
+    parameter [HR_MARGIN_WIDTH-1:0] H_Right_Margin_R10x7  =  11'b10010100000;   //1344-160=1184
+
 	
 	//Parameters for sync pulse, where the actual parameter is the sync pulse - 2
-	parameter [PULSE_WIDTH-1:0] H_Sync_Pulse_RD    =  8'b01011110;  //96-2=94
-	parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_RD    =  8'b00000000;  //2-2=0
+	parameter [PULSE_WIDTH-1:0] H_Sync_Pulse_RD    =  8'b01011110;  // 96-2=94
+	parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_RD    =  8'b00000000;  //  2-2=0
 	parameter [PULSE_WIDTH-1:0] H_Sync_Pulse_R8x6  =  8'b01111110;  //128-2=126
-    parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_R8x6  =  8'b00000010;  //4-2=2
+    parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_R8x6  =  8'b00000010;  //  4-2=2
 	parameter [PULSE_WIDTH-1:0] H_Sync_Pulse_R10x7 =  8'b10000110;  //136-2=134
-	parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_R10x7 =  8'b00000100;  //6-2=4
+	parameter [PULSE_WIDTH-1:0] V_Sync_Pulse_R10x7 =  8'b00000100;  //  6-2=4
 	
-	// Parameters for maximum counters, where maximum is the sum of the active+front porch+ back porch+ pulse width
-    parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_RD    =  12'b001000001100;   //524
-    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_RD    =  12'b001100100000;   //800
-    parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_R8x6  =  12'b001001110100;   //628
-    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_R8x6  =  12'b010000100000;   //1056
-	parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_R10x7 =  12'b001100100110;   //806
-    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_R10x7 =  12'b010010100000;   //1184
+	// Parameters for maximum counters, where maximum is the sum of the active+front porch + back porch + sync pulse
+    parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_RD    =  12'b001000001100;   // 480+11+  2+ 31= 524
+    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_RD    =  12'b001100100000;   // 640+16+ 96+ 48= 800
+    parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_R8x6  =  12'b001001110100;   // 600+ 1+  4+ 23= 628
+    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_R8x6  =  12'b010000100000;   // 800+40+128+ 88=1056
+	parameter [REZ_MAX_WIDTH-1:0] V_Count_Max_R10x7 =  12'b001100100110;   // 768+ 3+  6+ 29= 806
+    parameter [REZ_MAX_WIDTH-1:0] H_Count_Max_R10x7 =  12'b101010000000;   //1024+24+136+160=1344
--- VGA_Control/Width_Parameters.v
+++ VGA_Control/Width_Parameters.v
@@ -1,4 +1,12 @@
-parameter CONFIG_WIDTH = 2,
-parameter PORCH_WIDTH = 8,
-parameter PULSE_WIDTH = 8,
-parameter REZ_MAX_WIDTH = 12
+parameter CONFIG_WIDTH  	=  2,
+parameter COLOR_WIDTH   	=  4,
+parameter VL_MARGIN_WIDTH 	=  4,
+parameter PULSE_WIDTH   	=  8,
+parameter HL_MARGIN_WIDTH 	=  8,
+parameter VR_MARGIN_WIDTH 	= 10,
+parameter HR_MARGIN_WIDTH 	= 11,
+parameter REZ_MAX_WIDTH 	= 12,
+parameter DATA_WIDTH    	= 12
+
+
+
